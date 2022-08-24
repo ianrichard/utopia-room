308b9e2027258cdfba2589a5b224e4e6015cc38b
Ian Richard <smith.ianrichard@gmail.com>
2022-08-24 14:06:11 +0000

    wip

--- apps/editor/src/App.js
+++ apps/editor/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit hi <code>src/App.js</code> and save to reload.
+          Edit hi there <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
